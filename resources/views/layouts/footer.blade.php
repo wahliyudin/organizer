ea2b0191cc8d085b23ea92617d429d60b7b1c9c0
wahliyudin <wahliyudinzein@gmail.com>
2023-05-06 10:55:24 +0700

    Integrasi Template Admin

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,29 @@
+<div class="footer">
+    <div class="row justify-content-between align-items-center">
+        <div class="col">
+            <p class="fs-6 mb-0">&copy; Front. <span class="d-none d-sm-inline-block">2022
+                    Htmlstream.</span></p>
+        </div>
+        <div class="col-auto">
+            <div class="d-flex justify-content-end">
+                <ul class="list-inline list-separator">
+                    <li class="list-inline-item">
+                        <a class="list-separator-link" href="#">FAQ</a>
+                    </li>
+
+                    <li class="list-inline-item">
+                        <a class="list-separator-link" href="#">License</a>
+                    </li>
+
+                    <li class="list-inline-item">
+                        <button class="btn btn-ghost-secondary btn btn-icon btn-ghost-secondary rounded-circle"
+                            type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasKeyboardShortcuts"
+                            aria-controls="offcanvasKeyboardShortcuts">
+                            <i class="bi-command"></i>
+                        </button>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</div>
